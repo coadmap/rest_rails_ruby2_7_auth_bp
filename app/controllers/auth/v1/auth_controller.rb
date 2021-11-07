5ea645b945187f0f07e266886fc7d5196af55af9
uchidataku <bbb.rchp99@gmail.com>
2021-11-07 12:33:20 +0900

    ability追加

--- app/controllers/auth/v1/auth_controller.rb
+++ app/controllers/auth/v1/auth_controller.rb
@@ -11,6 +11,7 @@ module Auth
         ).try(:authenticate, resource_params[:password])
 
         if account
+          account.update!(last_sign_in_at: Time.zone.now)
           render json: {
             account: ::V1::AccountSerializer.new(account).as_json,
             token: account.jwt
@@ -21,7 +22,7 @@ module Auth
       end
 
       def sign_up
-        account = Account.create!(resource_params)
+        account = Account.create!(resource_params.merge(last_sign_in_at: Time.zone.now))
         render json: {
           account: ::V1::AccountSerializer.new(account).as_json,
           token: account.jwt
